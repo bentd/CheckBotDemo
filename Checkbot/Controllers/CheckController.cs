5d8eeb4d22a7c51139f11cb2561d2a53763863df
bentd <bentd.dev@gmail.com>
2021-01-24 19:59:33 -0500

    * package.json: * CheckController.cs: check controller edit
    * package-lock.json:

--- Checkbot/Controllers/CheckController.cs
+++ Checkbot/Controllers/CheckController.cs
@@ -12,33 +12,33 @@ namespace Checkbot.Controllers
     public class CheckController : Controller
     {
 
-        // GET: api/values
+        // GET: api/check
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/check/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return id.ToString();
         }
 
-        // POST api/values
+        // POST api/check
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/check/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/check/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
